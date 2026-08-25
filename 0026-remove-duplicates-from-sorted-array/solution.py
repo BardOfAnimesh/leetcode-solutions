3c27cf3659d1db10b71db4a3dab9ce0f859ab9e6
Animesh Das <animeshcode2oo6@gmail.com>
2026-08-25 18:36:23 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array - Runtime - 0 ms (100.00%), Memory - 13.7 MB (70.43%)

---
+++ 0026-remove-duplicates-from-sorted-array/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        l=0
+        r=1
+        for r in range(len(nums)):
+            if nums[l]!=nums[r]:
+                l+=1
+                nums[l]=nums[r]
+        return l+1
+
